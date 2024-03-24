0308566d1ca0e0866356c8452aa9492238db680b
Kaitlyn Liu <liu.kaitlync@gmail.com>
2024-03-24 19:47:10 -0400

    clear session on refresh

--- init.js
+++ init.js
@@ -1,11 +1,10 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 function initGemini() {
-console.log("hi")
     // Access your API key (see "Set up your API key" above)
     const genAI = new GoogleGenerativeAI("");
     console.log(genAI)
-    console.log("hi")
+    sessionStorage.clear();
 
 }
 
